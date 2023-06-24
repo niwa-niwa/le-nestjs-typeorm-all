e75d29bb3b9aebbc4b9fd61f18cc468465af194f
niwa-niwa <urban.hang274@gmail.com>
2023-06-24 19:53:34 +0900

    implemented apis of users to handling patch, delete, find

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  AfterInsert,
+  AfterRemove,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id:number;
+  id: number;
 
   @Column()
-  email:string;
+  email: string;
 
   @Column()
-  password:string;
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User with id ', this.id);
+  }
 }
