654cce5a1efe8550dc825edd035fd0ce53b35b85
niwa-niwa <urban.hang274@gmail.com>
2023-06-26 09:53:26 +0900

    implemented hash function for user password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,16 +14,20 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 // @UseInterceptors(new SerializeInterceptor(UserDto))
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    this.authService.signup(body.email, body.password);
   }
 
   @Get('/:id')
