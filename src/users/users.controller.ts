3386770a19d11cb7b99440fa0aa1c242a95c1f4d
niwa-niwa <urban.hang274@gmail.com>
2023-06-29 11:21:54 +0900

    implemented intercepter for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
   NotFoundException,
   Session,
+  UseInterceptors
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -16,19 +17,29 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from './user.entity';
+
 
 // @UseInterceptors(new SerializeInterceptor(UserDto))
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService,
   ) {}
 
+  // @Get('/whoami')
+  // whoAmI(@Session() session:any){
+  //   return this.usersService.findOne(session.userId)
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session:any){
-    return this.usersService.findOne(session.userId)
+  whoAmI(@CurrentUser() user:User){
+    return user
   }
 
   @Post('/signout')
