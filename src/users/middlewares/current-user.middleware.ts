74061c5b5d0e7b51f1e04997af61cdf3221ac99b
niwa-niwa <urban.hang274@gmail.com>
2023-07-08 17:58:59 +0900

    refactored type for express request

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,7 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
 
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
@@ -12,7 +20,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     if (userId) {
       const user = await this.usersService.findOne(userId);
 
-      // @ts-ignore
       req.currentUser = user;
     }
 
